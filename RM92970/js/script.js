a8754d9fb6086b7c11707b2f14ff1881164b4f61
evelywath <rm92970@fiap.com.br>
2022-08-01 11:52:06 -0300

    Criado objeto

--- RM92970/js/script.js
+++ RM92970/js/script.js
@@ -8,19 +8,37 @@
 //console.log("Olá Mundo!");
 
 //Declaração de variáveis
-var nome = "João"
-let idade = 20;
-const nacionalidade = "Brasileiro";
+//var nome = "João"
+//let idade = 20;
+//const nacionalidade = "Brasileiro";
 
 // Escopo de variáveis
 // Global
-var nome = "João";
+//var nome = "João";
 //Local
-let idade = 20;
+//let idade = 20;
 //CONSTANTE
-const nacionalidade = "Brasileiro";
+//const nacionalidade = "Brasileiro";
 
 //HOYSTING
-console.log(nome + " tem " + idade + " anos ");
-console.log(`${nome} tem ${idade} anos`);
+//console.log(nome + " tem " + idade + " anos ");
+//console.log(`${nome} tem ${idade} anos`);
 
+// Objeto literal
+const usuario = {
+    nome: "José",
+    idade: 20,
+    nacionalidade: "Angolaino",
+
+    cadastrar : function(nome, idade, nacionalidade){
+        this.nome = nome
+        this.idade = idade
+        this.nacionalidade = nacionalidade
+        console.log(`${this.nome} está cadastrado`);
+    }
+
+}
+
+usuario.cadastrar("José", 20, "Angolaino");
+
+console.log(usuario);
