fd2c7a431aed75596f879526f1a0195ff69d79b4
GuilhermeRocha0 <gui.rocha009@gmail.com>
2022-08-01 12:08:34 -0300

    Objeto liteiral

--- rm94771/js/script.js
+++ rm94771/js/script.js
@@ -9,13 +9,30 @@
 
 // Escopo de variáveis:
 // Global
-var nome = 'João'
+// var nome = 'João'
 // Local
-let idade = 20
+// let idade = 20
 // Constante
-const nacionalidade = 'Brasileiro'
+// const nacionalidade = 'Brasileiro'
 
 // Hoysting
-console.log(nome + ' tem ' + idade + ' anos')
+// console.log(nome + ' tem ' + idade + ' anos')
 // Interpolação
-console.log(`${nome} tem ${idade} anos`)
+// console.log(`${nome} tem ${idade} anos`)
+
+// Objeto literal
+const usuario = {
+  nome: '',
+  idade: 0,
+  nacionalidade: '',
+
+  cadastrar: function (nome, idade, nacionalidade) {
+    this.nome = nome
+    this.idade = idade
+    this.nacionalidade = nacionalidade
+    console.log(`${this.nome} está cadastrado`)
+  }
+}
+
+usuario.cadastrar('João', 20, 'Brasileiro')
+console.log(usuario)
