fbd85bb21394719000abad07cf2d8e6b116706f1
Bruno Cardoso <bruno.cardoso02@outlook.com>
2022-08-01 11:57:31 -0300

    Objeto literal!

--- rm93610/js/script.js
+++ rm93610/js/script.js
@@ -8,16 +8,32 @@
 //console.log('Olá mundo! ');
 
 //Hoysting
-console.log(nome + " tem " + idade + " anos");
+//console.log(nome + " tem " + idade + " anos");
 //Interpolação
-console.log(`${nome} tem ${idade} anos`);
+//console.log(`${nome} tem ${idade} anos`);
 
 //Declaração de variáveis
 
 //Global
-var nome = "João";
+//var nome = "João";
 //Local: util dentro de funções, loops e etc
-let idade = 20;
+//let idade = 20;
 //Constante, que não irá mudar com o programa
-const nacionalidade = "brasileiro";
+//const nacionalidade = "brasileiro";
 
+//Objeto literal
+const usuario = {
+    nome:"",
+    idade: 0,
+    nacionalidade: "",
+
+    cadastrar: function(nome, idade, nacionalidade){
+        this.nome = nome
+        this.idade = idade
+        this.nacionalidade = nacionalidade 
+        console.log(`${this.nome} está cadastrado`);
+    }
+}
+usuario.cadastrar("João", 20, "Brasileiro");
+
+console.log(usuario);
