2b53eacf15396ec76344c44655a07b13ae2d3ab3
PedroLu1z <rm95189@fiap.com.br>
2022-08-01 11:38:12 -0300

    escopo/HOYSTING/interpolação

---
+++ script.js
@@ -0,0 +1,22 @@
+//comentário de uma linha
+/*
+Comentário de multiplas linhas
+*/ 
+
+//Console.log para imprimir na tela
+//console.log('Olá Mundo!')
+
+//Declaração de Variaveis
+//ESCOPO de variáveis
+//Global
+var nome = 'João';
+
+//Local
+let idade = 20;
+
+//Constante
+const nacionalidade = 'brasileiro';
+
+//HOYSTING
+console.log(nome + ' tem' + idade + ' anos')
+console.log(`${nome} tem ${idade} anos`)    
