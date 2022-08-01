29e652205bc36e830966d7e8f2fe19aeac508390
VitorRT <barberino.italia@gmail.com>
2022-08-01 11:38:47 -0300

    Interpolação no JS

---
+++ rm95209/js/script.js
@@ -0,0 +1,17 @@
+// comentário XD
+
+//Escopo de variáveis
+
+
+//Hoysting
+console.log(`${nome} tem ${oldyear} anos.`) // vai dar erro
+
+//Global
+var nome = 'Vitu';
+//Local
+let oldyear = 18;
+//Constantes
+const nacionality = 'Brasileiro';
+
+//Hoysting
+console.log(`${nome} tem ${oldyear} anos.`)
