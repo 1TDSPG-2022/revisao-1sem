58cddfad49f7c9ee7169915ecbd868ff23bd5283
evelywath <rm92970@fiap.com.br>
2022-08-01 11:38:56 -0300

    Escopo/HOYSTING/CONST

--- RM92970/js/script.js
+++ RM92970/js/script.js
@@ -5,4 +5,22 @@
  * 
  */
 //Console.log() Serve para imprimir na tela
-console.log("Olá mundo!")
+//console.log("Olá Mundo!");
+
+//Declaração de variáveis
+var nome = "João"
+let idade = 20;
+const nacionalidade = "Brasileiro";
+
+// Escopo de variáveis
+// Global
+var nome = "João";
+//Local
+let idade = 20;
+//CONSTANTE
+const nacionalidade = "Brasileiro";
+
+//HOYSTING
+console.log(nome + " tem " + idade + " anos ");
+console.log(`${nome} tem ${idade} anos`);
+
