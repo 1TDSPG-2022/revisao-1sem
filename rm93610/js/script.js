dfad0c34953986d852d20afca300049232b3f3f3
Bruno Cardoso <bruno.cardoso02@outlook.com>
2022-08-01 11:38:34 -0300

    Escopo / Hoysting / Interpolação

--- rm93610/js/script.js
+++ rm93610/js/script.js
@@ -5,4 +5,19 @@
 
 //console.log('Serve para imprimir na tela')
 
-console.log('Olá mundo! ');
+//console.log('Olá mundo! ');
+
+//Hoysting
+console.log(nome + " tem " + idade + " anos");
+//Interpolação
+console.log(`${nome} tem ${idade} anos`);
+
+//Declaração de variáveis
+
+//Global
+var nome = "João";
+//Local: util dentro de funções, loops e etc
+let idade = 20;
+//Constante, que não irá mudar com o programa
+const nacionalidade = "brasileiro";
+
