89ef6bccb4194e99e0791b62e4b501ab65586230
feebueno <RM94667@fiap.com.br>
2022-08-01 11:51:48 -0300

    Objeto Literal

--- rm94667/js/script.js
+++ rm94667/js/script.js
@@ -1,1 +1,18 @@
-console.log("Olá Mundo!")
+//console.log("Olá Mundo!")
+
+const usuario = {
+    nome : "",
+    idade : 0,
+    nacionalidade : "",
+
+    cadastrar : function(nome, idade, nacionalidade){
+        this.nome = nome;
+        this.idade = idade;
+        this.nacionalidade = nacionalidade
+        console.log('${this.nome} está cadastrado');
+    }
+}
+
+usuario.cadastrar("Bueno", 18, "Brasileiro");
+console.log(usuario);
+
