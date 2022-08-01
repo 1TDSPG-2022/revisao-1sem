10ed8bbd8553f50b79bbd64554e9664f4a2a88cf
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-01 11:51:07 -0300

    Objeto Literal!

--- pf0670/js/script.js
+++ pf0670/js/script.js
@@ -6,20 +6,44 @@
 //Console.log() Serve para imprimir na tela
 //console.log("Olá Mundo!");
 
-//HOYSTING
-console.log(nome + " tem "  + " anos");
-//INTERPOLAÇÃO
-console.log(`${nome} tem anos`);
+// //HOYSTING
+// nome = "Hoysting";
+// console.log(nome + " tem "  + " anos");
+// //INTERPOLAÇÃO
+// console.log(`${nome} tem anos`);
 
-//Declaração de variáveis
+// //Declaração de variáveis
 
-//Escopo de variáveis
-//Global
-var nome = "João";
-//Local
-let idade = 20;
-//COSTANTE
-const nacionalidade = "Brasileiro";
+// //Escopo de variáveis
+// //Global
+// var nome = "João";
+// //Local
+// let idade = 20;
+// //COSTANTE
+// const nacionalidade = "Brasileiro";
+
+//Objeto literal
+const usuario = {
+    
+    nome:"",
+    idade:0,
+    nacionalidade:"",
+
+    cadastrar : function(nome, idade, nacionalidade){
+        this.nome = nome
+        this.idade = idade
+        this.nacionalidade = nacionalidade
+        console.log(`${this.nome} está cadastrado`);
+    }
+    
+}
+
+usuario.cadastrar("João", 20, "Brasileiro");
+console.log(usuario);
+
+
+
+
 
 
 
