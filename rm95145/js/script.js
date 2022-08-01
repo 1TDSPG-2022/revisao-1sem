dcdbd056e38045a01dfd5d017822ed876bba893a
IsaHatamiya <rm95145@fiap.com.br>
2022-08-01 11:43:35 -0300

    adições

--- rm95145/js/script.js
+++ rm95145/js/script.js
@@ -4,4 +4,23 @@
  */
 
 //Console.log() Serve para imprimir na tela
-console.log("Olá Mundo!");
+//console.log("Olá Mundo!");
+
+//Hoysting
+console.log(nome + " tem " + " anos ");
+//Interpolação
+console.log(`${nome} tem anos`);
+
+//Declaraçõ de variáveis
+
+//Escope de variáveis
+//Global
+var nome = "João";
+//Local
+let idade = 20;
+//Constante
+const nacionalidade = "Brasileiro";
+
+
+
+
