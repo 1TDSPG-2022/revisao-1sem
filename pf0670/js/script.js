c00b33cc8883b4ee807b033abacc30987c3d50f9
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-01 11:37:41 -0300

    Escopo/ HOYSTING / ITERPOLAÇÃO

--- pf0670/js/script.js
+++ pf0670/js/script.js
@@ -4,4 +4,22 @@
  */
 
 //Console.log() Serve para imprimir na tela
-console.log("Olá Mundo!");
+//console.log("Olá Mundo!");
+
+//HOYSTING
+console.log(nome + " tem "  + " anos");
+//INTERPOLAÇÃO
+console.log(`${nome} tem anos`);
+
+//Declaração de variáveis
+
+//Escopo de variáveis
+//Global
+var nome = "João";
+//Local
+let idade = 20;
+//COSTANTE
+const nacionalidade = "Brasileiro";
+
+
+
