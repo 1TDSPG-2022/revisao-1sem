a23f3d586b7ee83ee1028417fbf83395d30beef8
Luis <rm93025@fiap.com.br>
2022-08-01 11:58:02 -0300

    Adicionado function cadastro

--- rm93205/js/script.js
+++ rm93205/js/script.js
@@ -4,4 +4,40 @@
  */
 
 //Console.log() Serve para imprimir na tela
-console.log("Ola mundo!")
+//console.log("Ola mundo!")
+
+//HOYSTING 
+//console.log(nome + " tem " + idade + " anos");
+//INTERPOLACAO
+//console.log(`${nome} tem ${idade} anos`);
+
+//Declaracao de variaves
+
+//Escopo de variaveis
+//Global
+//var nome = "Joao";
+//Local
+//let idade = 20;
+//Constante 
+//const nacionalidade = "Brasileiro";
+
+//Objeto literal 
+const usuario = {
+
+    nome:'',
+    idade:'',
+    nacionalidade:'',
+
+    cadastrar : function(nome, idade, nacionalidade){
+        this.nome = 'Luis'
+        this.idade = 20
+        this.nacionalidade = nacionalidade
+        console.log(`${this.nome} esta cadastrado`);
+    }
+}
+
+usuario.cadastrar("Joao", 20, "Brasileiro")
+
+console.log(usuario);
+
+//
