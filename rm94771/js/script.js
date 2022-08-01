936378471176b16b1a3b2c03c925d6db439cccb0
GuilhermeRocha0 <gui.rocha009@gmail.com>
2022-08-01 11:38:15 -0300

    Escopo/ HOYSTING/ Interpolação

--- rm94771/js/script.js
+++ rm94771/js/script.js
@@ -3,4 +3,19 @@
  * Comentário de múltiplas linhas
  */
 // console.log() Serve para imprimir na tela
-console.log('Olá Mundo!')
+// console.log('Olá Mundo!')
+
+// Declaração de variáveis
+
+// Escopo de variáveis:
+// Global
+var nome = 'João'
+// Local
+let idade = 20
+// Constante
+const nacionalidade = 'Brasileiro'
+
+// Hoysting
+console.log(nome + ' tem ' + idade + ' anos')
+// Interpolação
+console.log(`${nome} tem ${idade} anos`)
