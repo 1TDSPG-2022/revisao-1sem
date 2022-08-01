fa821fa109c08ed50dc0cc7edc52dd70ec5108ff
Guilherme Menezes <101153274+GuilhermeMenezesdaSilva@users.noreply.github.com>
2022-08-01 11:38:33 -0300

    Escopo/Hoysting/ Interpolação

--- RM89384/js/script.js
+++ RM89384/js/script.js
@@ -4,4 +4,19 @@
  */
 
 //Console.log() - Serve para imprimir telas
-console.log("Olá,Mundo")
+//console.log("Olá,Mundo")
+
+//HOYSTING
+console.log(nome + " tem " + " anos");
+//INTERPOLAÇÃO
+console.log(`${Nnome} tem anos`)
+
+//Declaração de varaveis 
+
+//Escopo de variáveis 
+//Global 
+var nome = "João";
+//Local
+let idade = 20;
+//Constante 
+const nacionalidade = "Brasileiro"
