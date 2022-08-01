bb5d590081c1b320b9f906ea8c59798e7e5831f2
fabriziofbarros <fabriziofbarros@gmail.com>
2022-08-01 11:38:03 -0300

    Escopo/ HOYSTING / Interpolação

--- rm95320/js/script.js
+++ rm95320/js/script.js
@@ -1,1 +1,16 @@
 console.log("Olá Mundo!")
+
+//Global
+var nome = "Fabrizio";
+
+//Local
+let idade = 19;
+
+//Constante
+const nacionalidade = "Brasileiro";
+
+//HOYSTING
+console.log(nome + " tem " + idade + " anos")
+//Interpolação
+console.log(`${nome} tem ${idade} anos`)
+
