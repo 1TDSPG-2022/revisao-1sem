de64c312f0293a09cd8a5bbd817755c60799a2cc
PedroLu1z <rm95189@fiap.com.br>
2022-08-01 11:51:57 -0300

    criação de um objeto literal

--- script.js
+++ script.js
@@ -9,14 +9,28 @@ Comentário de multiplas linhas
 //Declaração de Variaveis
 //ESCOPO de variáveis
 //Global
-var nome = 'João';
-
+//var nome = 'João';
 //Local
-let idade = 20;
-
+//let idade = 20;
 //Constante
-const nacionalidade = 'brasileiro';
-
+//const nacionalidade = 'brasileiro';
 //HOYSTING
-console.log(nome + ' tem' + idade + ' anos')
-console.log(`${nome} tem ${idade} anos`)    
+//console.log(nome + ' tem' + idade + ' anos')
+//console.log(`${nome} tem ${idade} anos`)
+
+//objeto Literal
+
+const usuario = {
+    nome: 'Pedro',
+    Idade:0,
+    nacionalidade: 'Brasileiro',
+
+    cadastrar: function(nome, idade, nacionalidade) {
+        this.nome = nome
+        this.idade = idade
+        this.nacionalidade = nacionalidade
+        console.log(`${this.nome} está cadastrado`);
+    }
+}
+usuario.cadastrar('Joao', 22, 'Alemão')
+console.log(usuario)
