bb241361eaf529807930d3450ab1656c7fd9d018
brunoecp <rm95315@fiap.com.br>
2022-08-01 23:13:04 -0300

    complementação

--- rm95315/js/script.js
+++ rm95315/js/script.js
@@ -2,8 +2,36 @@
 /* 
 comentario de multiplaslinhas
 */
-console.log("olá")
-const button = document.getElementById("btnEnviar")
-button.addEventListener(onclick,function(){
-    alert("olá")
-})
+//console.log() serve para imprimir na tela
+//cosole.log(Olá Mundo!);
+
+//HOYSTING
+//console.log(nome + " tem " + " anos");
+//INTERPOLAÇÃO
+//console.log(`${nome} tem anos`);
+
+//Declaração de variáveis
+
+//Escopo de variáveis
+//Global
+//var nome = "João";
+//Local
+//let idade= 20;
+//Constante
+//const nacionalidade = "Brasileiro";
+
+//Objeto litral
+const usuario = {
+    nome:"",
+    idade: 0,
+    nacionalidade:"",
+
+    cadastrar : function(){
+        this.nome = nome
+        this.idade= idade
+        this.nacionalidade = nacionalidade
+        console.log(`${this.nome} está cadastrado`);
+    }
+}
+usuario.cadastrar("joão", 20, "Brasileiro")
+console.log(usuario)
