8bfc2a29ad518e8bd56ea8e1ac48eefc5b8d93eb
isabellapiol1 <rm93960@fiap.com.br>
2022-08-04 20:46:33 -0300

    Objeto Literal!

--- rm93960/js/script.js
+++ rm93960/js/script.js
@@ -8,18 +8,37 @@
 //console.log("Olá mundo!");
 
 //HOYSTING
-console.log(nome + " tem " + " anos ");
+//nome = "Hoysting"
+//console.log(nome + " tem " + " anos ");
 //INTERPOLAÇÃO
-console.log(`${nome} tem anos`);
+//console.log(`${nome} tem anos`);
 
 //Declaração de variaveis 
 
 //Escopo das variaveis 
 //Global
-var nome = "João";
+//var nome = "João";
 //Local
-let idade = 20; //Usa em escopo fechados
+//let idade = 20; //Usa em escopo fechados
 //Constante
-const nacionalidade = "Brasileiro";
+//const nacionalidade = "Brasileiro";
+
+//Objeto literal
+const usuario = {
+    nome:"",
+    idade:0,
+    nacionaliadade:"",
+
+    cadastrar : function(nome, idade, nacionaliadade){
+        this.nome = nome
+        this.idade = idade
+        this.nacionaliadade = nacionaliadade
+        console.log(`${this.nome} está cadastrado`);
+        
+    }
+}
+
+usuario.cadastrar("João", 20, "Brasileiro");
+console.log(usuario);
 
 
