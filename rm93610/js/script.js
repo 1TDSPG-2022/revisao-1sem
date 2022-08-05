2b1fe00fcdd3818b12e5779a13b840ca22309a4a
Bruno Cardoso <bruno.cardoso02@outlook.com>
2022-08-05 08:50:01 -0300

    Exercicio basico

--- rm93610/js/script.js
+++ rm93610/js/script.js
@@ -22,18 +22,33 @@
 //const nacionalidade = "brasileiro";
 
 //Objeto literal
-const usuario = {
-    nome:"",
-    idade: 0,
-    nacionalidade: "",
+const tarefas = {
+    nmTask:"",
+    dtTask: 0,
+    descTask: "",
 
-    cadastrar: function(nome, idade, nacionalidade){
-        this.nome = nome
-        this.idade = idade
-        this.nacionalidade = nacionalidade 
-        console.log(`${this.nome} está cadastrado`);
+    cadastrar: function(nmTask, dtTask, descTask){
+        this.nmTask = nmTask
+        this.dtTask = dtTask
+        this.descTask = descTask 
+        console.log(`${this.nmTask} está cadastrado`);
     }
 }
-usuario.cadastrar("João", 20, "Brasileiro");
 
+tarefas.cadastrar("João", "Passear com os Dogs!", "Brasileiro");
 console.log(usuario);
+
+//const btn = document.getElementById("btnEnviar")
+
+btn.addEventListener("click", function(){
+    let inputTask = document.querySelectorAll("input[type=text], input[type=date]")
+
+    for(let x = 0; x < inputTask.length ; x++){
+
+        tarefas.cadastrar(inputTask[0].value, inputTask[1].value, inputTask[2].value);
+        console.log(tarefas)
+    }
+
+    
+    usuario.cadastrar()
+})
