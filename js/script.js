7d66c7fb5ecff32c20694aecf9374991b7137215
vSeiji <rm93380@fiap.com.br>
2022-08-05 10:27:52 -0300

    Alterações da aula de 05/08

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
 /*
 Console.log() - Serve para imprimir na tela
 */
-console.log("Olá mundo!")
+
+// Objeto Literal
+const tarefas = {
+
+    nmTask:"",
+    dtTask:"",
+    descTask:"",
+
+    cadastrar: function(nmTask, dtTask, descTask) {
+        this.nmTask = nmTask
+        this.dtTask = dtTask
+        this.descTask = descTask
+        console.log(`${this.nmTask} está cadastrada`);
+    }
+}
+
+tarefas.cadastrar("João","Passear com os dogs","Brasileiro");
+console.log(tarefas);
+
+const btn = document.getElementById("btnEnviar")
+
+btn.addEventListener("click", function() {
+    let inputTask = document.querySelectorAll("input[type]=text,input[type]=date")
+    
+        tarefas.cadastrar(inputTask[0].value, inputTask[1].value, inputTask[2].value)
+        console.log(tarefas)
+        
+})
