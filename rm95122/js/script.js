c08157fb9de1434bccc1a5201d94b3ebf5656f75
FlipOwz <rm95122@fiap.com.br>
2022-08-05 08:29:31 -0300

    scripit melhorado

--- rm95122/js/script.js
+++ rm95122/js/script.js
@@ -1,40 +1,25 @@
-//Comentário de uma linha
-/**
- * Comentário de múltiplas linhas
- */
 
-//Console.log() Serve para imprimir na tela
-//console.log("Olá Mundo!");
+const tarefas = {
 
-//Hoysting
-//console.log(nome + " tem " + " anos ");
-//Interpolação
-//console.log(${nome} tem anos);
+    nmTask:"",
+    dtTask:0,
+    descTask:"",
 
-//Declaraçõ de variáveis
-
-//Escope de variáveis
-//Global
-//var nome = "João";
-//Local
-//let idade = 20;
-//Constante
-//const nacionalidade = "Brasileiro";
-
-//Objeto literal
-const usuario = {
-
-    nome:"",
-    idade:0,
-    nacionalidade:"",
-
-    cadastrar : function(nome, idade, nacionalidade){
-        this.nome = nome
-        this.idade = idade
-        this.nacionalidadSe = nacionalidade
-        console.log($(this.nome) 'está cadastrado' );
+    cadastrar : function(nmTask, dtTask, descTask){
+        this.nmTask = nmTask
+        this.dtTask = dtTask
+        this.descTask = descTask
+        console.log(`${this.nmTask} está cadastrado`);
     }
 }
 
-usuario.cadastrar("João", 20, "Brasileiro");
-console.log(usuario);
+tarefas.cadastrar("João", "Passear com os Dogs!", "Brasileiro");
+console.log(tarefas);
+
+const btn = document.getElementById("btnEnviar")
+
+btn.addEventListener("click",function(){
+    let inputTask = document.querySelectorAll("input[type=text],input[type=date]")
+    
+    tarefas.cadastrar()
+})
